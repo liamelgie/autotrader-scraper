480231818094f960e86f83de2fc79afddbcfb1a0
Liam Elgie <liamelgie@gmail.com>
2019-01-28 23:51:59 +0000

    Initial commit

---
+++ tests/fetch-results.js
@@ -0,0 +1,4 @@
+const AutoTraderScraper = require('../autoscraper.js')
+
+let a = new AutoTraderScraper()
+a.fetchResults('https://www.autotrader.co.uk/car-search?radius=1500&postcode=b987wy&onesearchad=Used&onesearchad=Nearly%20New&onesearchad=New')
