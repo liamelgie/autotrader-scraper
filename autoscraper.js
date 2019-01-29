8492db753ca5cee6cc3b9a805ee60dcfff4483a9
Liam Elgie <liamelgie@gmail.com>
2019-01-29 01:07:25 +0000

    Add Ad object

--- autoscraper.js
+++ autoscraper.js
@@ -3,4 +3,26 @@ class AutoTraderScraper {
 
   }
 }
+class Ad {
+  constructor(node) {
+    if (!node) return null
+    this.$ = cheerio.load(node)
+    this.price = this.$('.vehicle-price').text()
+    this.title = this.$('.listing-title').text()
+    /* FIX: the following cannot be used as the data provided by users is unpredictable. This must be addressed.
+     * this.year = this.$('.listing-key-specs ').find('li').first().text()
+     * this.body = this.$('.listing-key-specs ').find('li').first().next().text()
+     * etc, etc
+     *
+     * The following is a working alternative but does not allow for named referencing of each spec.
+     *
+     */
+    this.keySpecs = this.$('.listing-key-specs ').find('li').map((i, el) => {
+      return this.$(el).text().replace(/\n/g, '').trim()
+    }).get()
+    this.description = this.$('.listing-description').text()
+    this.location = this.$('.seller-location').text().replace(/\n/g, '').trim()
+    this.img = this.$('img').attr('src')
+  }
+}
 module.exports = AutoTraderScraper
