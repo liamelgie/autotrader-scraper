12c87057ad73ea6e37d54215ccb49f9c99dfa963
Liam Elgie <liamelgie@gmail.com>
2019-01-29 01:16:16 +0000

    Fix test returning before fetch completes

--- tests/fetch-results.js
+++ tests/fetch-results.js
@@ -1,4 +1,8 @@
 const AutoTraderScraper = require('../autoscraper.js')
 
-let a = new AutoTraderScraper()
-a.fetchResults('https://www.autotrader.co.uk/car-search?radius=1500&postcode=b987wy&onesearchad=Used&onesearchad=Nearly%20New&onesearchad=New')
+async function init() {
+  let a = new AutoTraderScraper()
+  console.log(await a.fetchResults('https://www.autotrader.co.uk/car-search?radius=1500&postcode=b987wy&onesearchad=Used&onesearchad=Nearly%20New&onesearchad=New'))
+}
+
+init()
