6d1bac790ac90b5b163d01708b09f52f83d35ecf
Liam Elgie <liamelgie@gmail.com>
2019-01-29 16:45:52 +0000

    Update fetchResults to fetchListings

--- tests/fetch-results.js
+++ tests/fetch-results.js
@@ -2,7 +2,7 @@ const AutoTraderScraper = require('../autoscraper.js')
 
 async function init() {
   let a = new AutoTraderScraper()
-  console.log(await a.fetchResults('https://www.autotrader.co.uk/car-search?radius=1500&postcode=b987wy&onesearchad=Used&onesearchad=Nearly%20New&onesearchad=New'))
+  console.log(await a.fetchListings('https://www.autotrader.co.uk/car-search?radius=1500&postcode=b987wy&onesearchad=Used&onesearchad=Nearly%20New&onesearchad=New'))
 }
 
 init()
