a343ddafdb8d3d13a3ac4365ebe67cec5b774667
Liam Elgie <liamelgie@gmail.com>
2019-01-31 02:47:13 +0000

    Update to use almost all possible search parameters

--- tests/search.js
+++ tests/search.js
@@ -4,16 +4,50 @@ async function init() {
   let a = new AutoTraderScraper()
   let criteria = {
     location: {
-      radius: 25,
-      location: "WC1X0HS"
+      radius: 1500,
+      postcode: "WC1X0HS"
     },
-    condition: 'Used',
+    make: 'FORD',
+    model: 'KUGA',
+    variant: 'Titanium',
+    condition: ['New', 'Nearly New', 'Used'],
     price: {
       min: '1000',
-      max: '10000'
+      max: '40000'
     },
-    make: 'NISSAN',
-    model: 'Skyline'
+    year: {
+      min: '2001',
+      max: '2018'
+    },
+    mileage: {
+      min: '0',
+      max: '200000'
+    },
+    body: 'SUV',
+    fuel: {
+      type: 'Diesel',
+      consumption: 'OVER_30'
+    },
+    engine: {
+      min: '1.2',
+      max: '4.5'
+    },
+    acceleration: '8_TO_12',
+    gearbox: 'Manual',
+    drivetrain: 'Front Wheel Drive',
+    emissions: 'TO_200',
+    doors: '',
+    seats: {
+      min: '5',
+      max: '7'
+    },
+    insurance: '40U',
+    tax: 'TO_295',
+    colour: 'Black',
+    excludeWriteOffs: false,
+    onlyWriteOffs: false,
+    customKeywords: '',
+    pageNumber: '1'
   }
   console.log(await a.search(criteria))
 }
