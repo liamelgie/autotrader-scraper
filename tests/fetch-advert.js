562ad92fa6cd7ae9bf5093dbe293bae0906d66b2
Liam Elgie <liamelgie@gmail.com>
2019-02-16 18:15:41 +0000

    Update to use new syntax

--- tests/fetch-advert.js
+++ tests/fetch-advert.js
@@ -2,7 +2,11 @@ const AutoTraderScraper = require('../autoscraper.js')
 
 async function init() {
   let a = new AutoTraderScraper()
-  console.log(await a.fetchAdvert('https://www.autotrader.co.uk/classified/advert/201901294419300?body-type=SUV&advertising-location=at_cars&postcode=b987wy&onesearchad=New&onesearchad=Nearly%20New&onesearchad=Used&sort=sponsored&radius=1501&page=1'))
+  await a.getAdvert('https://www.autotrader.co.uk/classified/advert/201902084745116?advertising-location=at_cars&sort=sponsored&radius=1500&postcode=b987wy&onesearchad=Used&onesearchad=Nearly%20New&page=1')
+
+  .then(advert => advert.literal)
+  .then(literal => console.log(literal))
+  await a.exit()
 }
 
 init()
